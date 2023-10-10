c174634f92208f714c45e99de0b64830b706f22f
dullbear1125 <124883981+dullbear1125@users.noreply.github.com>
2023-10-10 17:40:05 +0900

    fix2

--- Ch3/lib/text/HelloWorld.dart
+++ Ch3/lib/text/HelloWorld.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HelloPage extends StatefulWidget{
   late String title;
+  int count = 0;
 
   HelloPage({super.key, required String title}){
     this.title = title;
@@ -12,25 +13,40 @@ class HelloPage extends StatefulWidget{
 
 class _HelloPageState extends State<HelloPage> {
   String messge = 'Hello World';
+  //int count = 0;
 
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
+        title: Text(widget.title),
       ),
-      body: Text(
-        messge,
-        style: TextStyle(fontSize: 50),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              messge,
+              style: TextStyle(fontSize: 50),
+            ),
+            Text(
+              '${widget.count}',
+              style: TextStyle(fontSize: 50),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            messge = "헬로 월드";
-          });
-        }),
+        onPressed: ChangeCounter
+        ),
     );
   }
 
+void ChangeCounter(){
+    widget.count++;
+    setState(() {
+
+    });
+}
 
 }
