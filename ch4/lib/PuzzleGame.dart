38d30befd347351ef67cec5684b84d3c58a2fcfa
dullbear1125 <124883981+dullbear1125@users.noreply.github.com>
2023-10-17 17:52:09 +0900

    puzzle

--- ch4/lib/PuzzleGame.dart
+++ ch4/lib/PuzzleGame.dart
@@ -1,7 +1,59 @@
+import 'dart:math';
+
 class PuzzleGame {
-  List<int> value = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,0];
+  List<int> values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,0];
 
   int Getvalue(int index) {
-    return value[index];
+    return values [index];
+  }
+  void Shuffle(){
+    int index = 0;
+    int num, i;
+
+    while(index < 19){
+      num = Random().nextInt(19) + 1;
+
+      for(i=0; i<index; i++){
+        if(num == values[i]) break;
+      }
+      if(i == index){
+        values[index] =num;
+        index = index + 1;
+      }
+    }
+    values[19] = 0;
+  }
+
+  void SwapPosition(int index){
+    int zeroPosition = FindZeroPosition(index);
+    if (zeroPosition == -1) return;
+    values[zeroPosition] = values[index];
+    values[index] = 0;
+  }
+  int FindZeroPosition(int index){
+    int zeroIndex = -1;
+    int maxX = 4;
+    int maxY = 5;
+    int indexX = index % maxX;
+    int indexY = index ~/ maxX;
+
+    if(indexX != 0){
+      zeroIndex =indexY * maxX + indexX - 1;
+      if(values[zeroIndex] == 0) return zeroIndex;
+    }
+    if(indexX != maxX-1){
+      zeroIndex =indexY * maxX + indexX + 1;
+      if(values[zeroIndex] == 0) return zeroIndex;
+    }
+    if(indexY != 0){
+      zeroIndex =(indexY-1) * maxX + indexX;
+      if(values[zeroIndex] == 0) return zeroIndex;
+    }
+    if(indexY != maxY-1){
+      zeroIndex =(indexY+1) * maxX + indexX;
+      if(values[zeroIndex] == 0) return zeroIndex;
+    }
+
+    return zeroIndex;
   }
 }
--- ch4/lib/PuzzleUI.dart
+++ ch4/lib/PuzzleUI.dart
@@ -32,87 +32,91 @@ class _PuzzleUI extends State<PuzzleUI> {
         crossAxisCount: 4,
         children: [
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(0);},
             child: Text(GetButtonText(0)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(1);},
             child: Text(GetButtonText(1)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(2);},
             child: Text(GetButtonText(2)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(3);},
             child: Text(GetButtonText(3)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(4);},
             child: Text(GetButtonText(4)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(5);},
             child: Text(GetButtonText(5)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(6);},
             child: Text(GetButtonText(6)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(7);},
             child: Text(GetButtonText(7)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(8);},
             child: Text(GetButtonText(8)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(9);},
             child: Text(GetButtonText(9)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(10);},
             child: Text(GetButtonText(10)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(11);},
             child: Text(GetButtonText(11)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(12);},
             child: Text(GetButtonText(12)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(13);},
             child: Text(GetButtonText(13)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(14);},
             child: Text(GetButtonText(14)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(15);},
             child: Text(GetButtonText(15)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(16);},
             child: Text(GetButtonText(16)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(17);},
             child: Text(GetButtonText(17)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(18);},
             child: Text(GetButtonText(18)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){MoveButton(19);},
             child: Text(GetButtonText(19)),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: (){
+              setState(() {
+                pg.Shuffle();
+              });
+            },
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.cyan,
             ),
@@ -124,5 +128,11 @@ class _PuzzleUI extends State<PuzzleUI> {
         ],
       ),
     );
+  }
+
+  void MoveButton(int index){
+    setState(() {
+      pg.SwapPosition(index);
+    });
   }
 }
