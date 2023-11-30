04754ff0f958c84992d63390c25912e513112d8e
dullbear1125 <>
2023-11-30 17:33:23 +0900

    new

---
+++ ch10/lib/StopWatchPage2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StopWatchPage2 extends StatelessWidget {
+  const StopWatchPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
